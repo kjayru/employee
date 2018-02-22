ba4e0188422cb6c7bed810257cec575082ff2e39
= <wiltinoco@gmail.com>
2018-02-22 05:06:57 -0500

    Files developer

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <div class="nav-wrapper green">
+      <div class="container">
+        <router-link to="/" class="brand-logo">Administrador </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default{
+
+    apiKey: "AIzaSyBz0rFBp_hJaBv4jFTME9LbStyPxOxG3FI",
+    authDomain: "webapp-10b9a.firebaseapp.com",
+    databaseURL: "https://webapp-10b9a.firebaseio.com",
+    projectId: "webapp-10b9a",
+    storageBucket: "webapp-10b9a.appspot.com",
+    messagingSenderId: "54861963453"
+
+}
